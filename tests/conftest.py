18eff37de725ab0e116d3be67b10396a2492436f
necros <necros83@gmail.com>
2024-01-13 15:22:26 +0530

    fix import from conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config, browser
+from selene import browser
 from dotenv import load_dotenv
 from utils import attach
 
